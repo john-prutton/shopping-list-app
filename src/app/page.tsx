c645053e50d17e90d88b4c6f1890acc167214a63
John Prutton (PRTJOH016) <johnscon9@gmail.com>
2023-10-04 11:18:38 +0200

    refactor: remove unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import Link from "next/link"
-
-import { Section } from "@/components/layout/section"
-
-import { Button } from "@/components/ui/button"
-import { LeaveGroupDialog } from "@/components/groups/leave-dialog"
-import { CreateGroupDialog } from "@/components/groups/create-dialog"
 import { GroupSection } from "@/components/groups/section"
 
 export default function Home() {
