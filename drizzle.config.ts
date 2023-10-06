b1825302974c0bdd3a641daa57d1dc335ffa3a88
John Prutton (PRTJOH016) <johnscon9@gmail.com>
2023-10-06 13:12:41 +0200

    feat: enable multiple `.env` files

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,8 @@
-import type { Config } from "drizzle-kit";
+// For loading multiple env files
+import "dotenv-flow/config"
+
 import { env } from "@/lib/env.mjs";
+import type { Config } from "drizzle-kit";
 
 export default {
   schema: "./src/lib/db/schema",
--- src/lib/env.mjs
+++ src/lib/env.mjs
@@ -1,6 +1,6 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
-import "dotenv/config";
+
 
 export const env = createEnv({
   server: {
@@ -9,8 +9,8 @@ export const env = createEnv({
       .default("development"),
     DATABASE_URL: z.string().min(1),
     NEXTAUTH_SECRET: process.env.NODE_ENV === "production"
-        ? z.string().min(1)
-        : z.string().min(1).optional(),
+      ? z.string().min(1)
+      : z.string().min(1).optional(),
     NEXTAUTH_URL: z.preprocess(
       // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
       // Since NextAuth.js automatically uses the VERCEL_URL if present.
@@ -22,7 +22,7 @@ export const env = createEnv({
     GITHUB_CLIENT_SECRET: z.string().min(1),
   },
   client: {
-        // NEXT_PUBLIC_PUBLISHABLE_KEY: z.string().min(1),
+    // NEXT_PUBLIC_PUBLISHABLE_KEY: z.string().min(1),
   },
   // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
   // runtimeEnv: {
@@ -31,6 +31,6 @@ export const env = createEnv({
   // },
   // For Next.js >= 13.4.4, you only need to destructure client variables:
   experimental__runtimeEnv: {
-        // NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
+    // NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
   },
 });
