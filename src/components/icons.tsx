1ea8ec327c1c8455c3bee596fda6d5cdb4a389c6
John Prutton (PRTJOH016) <johnscon9@gmail.com>
2023-10-08 01:58:06 +0200

    refactor: remove unused icons

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,12 +1,8 @@
 export {
 	PlusIcon as AddIcon,
-	MenuIcon as MenuIcon,
-	LogInIcon as LogInIcon,
-	LogOutIcon as LogOutIcon,
-	Settings2Icon as EditIcon,
+	Settings2Icon as EditItemIcon,
 	UserX2Icon as LeaveIcon,
 	CopyIcon as CopyIcon,
-	FileEditIcon as EditItemIcon,
 	FilePlus2Icon as AddItemIcon,
 	FolderPlusIcon as AddGroupIcon,
 	FolderMinusIcon as LeaveGroupIcon,
--- src/components/layout/action-dialog.tsx
+++ src/components/layout/action-dialog.tsx
@@ -1,5 +1,3 @@
-import { AddIcon } from "@/components/icons"
-
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button"
 
