193537c8a00b31b5f12f93148e42e942c7bac0d9
John Prutton (PRTJOH016) <johnscon9@gmail.com>
2023-10-08 00:10:35 +0200

    style: includes icons for last commit

--- src/lib/icons.ts
+++ src/lib/icons.ts
@@ -5,5 +5,12 @@ export {
   LogOutIcon as LogOutIcon,
   Settings2Icon as EditIcon,
   UserX2Icon as LeaveIcon,
-  CopyIcon as CopyIcon
+  CopyIcon as CopyIcon,
+  FileEditIcon as EditItemIcon,
+  FilePlus2Icon as AddItemIcon,
+
+  FolderPlusIcon as AddGroupIcon,
+  FolderMinusIcon as LeaveGroupIcon,
+
+  UserPlusIcon as AddUserIcon,
 } from "lucide-react"
