819f01d44f424e4ecf11123feeef461b86798bb3
John Prutton (PRTJOH016) <johnscon9@gmail.com>
2023-10-08 18:01:18 +0200

    potential-fix: change libsql connection type to web

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,15 @@
 // For loading multiple env files
 import "dotenv-flow/config"
 
-import { env } from "@/lib/env.mjs";
-import type { Config } from "drizzle-kit";
+import { env } from "@/lib/env.mjs"
+import type { Config } from "drizzle-kit"
 
 export default {
-  schema: "./src/lib/db/schema",
-  out: "./src/lib/db/migrations",
-  driver: "turso",
-  dbCredentials: {
-    url: env.DATABASE_URL,
-    authToken: env.DATABASE_AUTH_TOKEN
-  }
-} satisfies Config;
+	schema: "./src/lib/db/schema",
+	out: "./src/lib/db/migrations",
+	driver: "turso",
+	dbCredentials: {
+		url: env.DATABASE_URL,
+		authToken: env.DATABASE_AUTH_TOKEN,
+	},
+} satisfies Config
--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,7 +1,7 @@
-import { drizzle } from 'drizzle-orm/libsql';
-import { createClient } from '@libsql/client';
+import { drizzle } from "drizzle-orm/libsql"
+import { createClient } from "@libsql/client/web"
 
-import { env } from '../env.mjs';
+import { env } from "../env.mjs"
 
 import * as GroupSchema from "@/lib/db/schema/groups"
 import * as AuthSchema from "@/lib/db/schema/auth"
@@ -9,14 +9,14 @@ import * as UserGroupSchema from "@/lib/db/schema/users-on-groups"
 import * as ItemSchema from "@/lib/db/schema/items"
 
 export const sqlite = createClient({
-  url: env.DATABASE_URL,
-  authToken: env.DATABASE_AUTH_TOKEN
-});
+	url: env.DATABASE_URL,
+	authToken: env.DATABASE_AUTH_TOKEN,
+})
 export const db = drizzle(sqlite, {
-  schema: {
-    ...GroupSchema,
-    ...AuthSchema,
-    ...UserGroupSchema,
-    ...ItemSchema
-  }
-});
+	schema: {
+		...GroupSchema,
+		...AuthSchema,
+		...UserGroupSchema,
+		...ItemSchema,
+	},
+})
